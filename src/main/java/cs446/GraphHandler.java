20baa0e5b820830714ea75478cb93facd5fea1f3
Shyam Upadhyay <shyamupa@gmail.com>
2013-11-25 16:35:51 -0600

    Made changes to reflect Concept API

--- src/main/java/cs446/GraphHandler.java
+++ src/main/java/cs446/GraphHandler.java
@@ -9,39 +9,51 @@ import cs446.WNWrapper;
 import edu.mit.jwi.item.ISynset;
 import edu.mit.jwi.item.POS;
 
+
 public class GraphHandler {
+	public enum Metric
+	{
+		LeacockChodorow(1),
+		Lesk(2),
+		WuPalmer(3),
+		Resnick(4);
+		private int value;
+		private Metric(int val){
+			this.value=val;
+		}
+		public int getVal(){
+			return value;
+		}
+	};
 	private List<AmbWord> words;
 	final static int WIN_MAX=5;	// should take this as command line arg later
 	private GraphCentralityScorer gcScorer;
 	private WNWrapper wn;
+	private Metric metric;
 	public GraphHandler(List<AmbWord> words, int seq_length) {
 		this.words=words;
 		wn=new WNWrapper("data/WordNet-3.0/dict");
 	}
-	public Graph<ISynset> CreateGraph(List<AmbWord> words)
+	public Graph<Integer> CreateGraph(List<AmbWord> words)
 	{
 		
 		
-		List<Vertex<ISynset>> vertices=new ArrayList<Vertex<ISynset>>();
+		List<Vertex<Integer>> vertices=new ArrayList<Vertex<Integer>>();
 		List<Edge> edges=new ArrayList<Edge>();
 		
-		Map<AmbWord,List<Vertex<ISynset>>> map=new HashMap<AmbWord,List<Vertex<ISynset>>>();
+		Map<AmbWord,List<Vertex<Integer>>> map=new HashMap<AmbWord,List<Vertex<Integer>>>();
 		// hashmap storing mapping from ambword to each of its candidate nodes in the graph
 		
 		// init book-keeping
 		for(AmbWord w: words)
 		{
 			String pos = w.getPos();
-			map.put(w,new ArrayList<Vertex<ISynset>>());
+			map.put(w,new ArrayList<Vertex<Integer>>());
 			for(int i=0;i<wn.getClusterRange(w.getWord());i++)
 			{
-				List<ISynset>senses=wn.getAllSynsetsFromCluster(w.getWord(),pos,i);
-				for(ISynset sense: senses)
-				{
-					Vertex<ISynset> v = new Vertex<ISynset>(sense);
-					vertices.add(v);
-					map.get(w).add(v);	// add v to vertices under w
-				}
+				Vertex<Integer> v = new Vertex<Integer>(i);
+				vertices.add(v);
+				map.get(w).add(v);	// add v to vertices under w
 			}
 		}
 		double edgeCost=0.0;
@@ -53,11 +65,12 @@ public class GraphHandler {
 					continue;
 				if(w1.getID() - w2.getID() > WIN_MAX)
 					break;
-				for(Vertex s1: map.get(w1))
+				for(Vertex<Integer> s1: map.get(w1))
 				{
-					for(Vertex s2: map.get(w2))
+					for(Vertex<Integer> s2: map.get(w2))
 					{
-//						edgeCost=dependency(w1,w2,s1.getVal(),s2.getVal());
+						
+						edgeCost=wn.dependency(w1,w2,s1.getVal(),s2.getVal(),metric.getVal());
 						if(edgeCost>0)
 						{
 							edges.add(new Edge(s1,s2,edgeCost));
@@ -67,7 +80,7 @@ public class GraphHandler {
 			}
 		}
 		
-	return new Graph<ISynset>(edges, vertices);
+	return new Graph<Integer>(edges, vertices);
 	}
 	public void ScoreVertices(Graph<ISynset> g)
 	{
